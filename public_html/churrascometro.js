e55911b223941dc230176f45628db94ca89dba2c
Ramon Chiara <ramonchiara@gmail.com>
2018-03-28 15:24:52 -0300

    Adição dos produtos à lista de compras.

--- public_html/churrascometro.js
+++ public_html/churrascometro.js
@@ -5,11 +5,62 @@ churras.controller('principal', function ($scope, $http) {
     $scope.carnes = [];
     $scope.bebidas = [];
     $scope.outros = [];
-    
+
     $http.get('produtos.json').then(function (response) {
         $scope.carnes = response.data.carnes;
         $scope.bebidas = response.data.bebidas;
         $scope.outros = response.data.outros;
     });
 
+    $scope.compras = [];
+
+    $scope.comprarCarne = function () {
+        var carnes = $('#carnes').val();
+        var carnesQtd = $('#carnes-qtd').val();
+
+        var p = JSON.parse(carnes);
+        var q = parseFloat(carnesQtd);
+
+        if (q > 0) {
+            $scope.compras.push({
+                tipo: 'carnes',
+                produto: p,
+                quantidade: q
+            });
+        }
+    };
+
+    $scope.comprarBebida = function () {
+        var bebidas = $('#bebidas').val();
+        var bebidasQtd = $('#bebidas-qtd').val();
+
+        var p = JSON.parse(bebidas);
+        var q = parseFloat(bebidasQtd);
+
+        if (q > 0) {
+            $scope.compras.push({
+                tipo: 'bebidas',
+                quantidade: q,
+                produto: p
+            });
+        }
+    };
+
+    $scope.comprarOutro = function () {
+        var outros = $('#outros').val();
+        var outrosQtd = $('#outros-qtd').val();
+
+        var p = JSON.parse(outros);
+        var q = parseFloat(outrosQtd);
+
+        if (q > 0) {
+            $scope.compras.push({
+                tipo: 'outros',
+                quantidade: q,
+                produto: p
+            });
+        }
+    };
+
 });
+
