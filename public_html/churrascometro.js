1dc323339bda191ced04de3ee0d98886734d97c1
Ramon Chiara <ramonchiara@gmail.com>
2018-04-01 01:24:12 -0300

    Calculando o total.

--- public_html/churrascometro.js
+++ public_html/churrascometro.js
@@ -17,12 +17,14 @@ churras.controller('principal', function ($scope, $http) {
     $scope.comprarCarne = function () {
         var p = $scope.carneSelecionada;
         var q = $scope.carneQtd;
+        var v = valor(p, $scope.carnes);
 
         if (p !== undefined && q > 0) {
             $scope.compras.push({
                 tipo: 'carnes',
                 produto: p,
-                quantidade: q
+                quantidade: q,
+                valor: v
             });
         }
     };
@@ -30,12 +32,14 @@ churras.controller('principal', function ($scope, $http) {
     $scope.comprarBebida = function () {
         var p = $scope.bebidaSelecionada;
         var q = $scope.bebidaQtd;
+        var v = valor(p, $scope.bebidas);
 
         if (p !== undefined && q > 0) {
             $scope.compras.push({
                 tipo: 'bebidas',
                 produto: p,
-                quantidade: q
+                quantidade: q,
+                valor: v
             });
         }
     };
@@ -43,14 +47,31 @@ churras.controller('principal', function ($scope, $http) {
     $scope.comprarOutro = function () {
         var p = $scope.outroSelecionado;
         var q = $scope.outroQtd;
+        var v = valor(p, $scope.outros);
 
         if (p !== undefined && q > 0) {
             $scope.compras.push({
                 tipo: 'outros',
                 produto: p,
-                quantidade: q
+                quantidade: q,
+                valor: v
             });
         }
+    };
+
+    var valor = function (nome, lista) {
+        var valor = 0;
+
+        for (var i = 0; i < lista.length; i++) {
+            var p = lista[i];
+
+            if (p.nome === nome) {
+                valor = p.valor;
+                break;
+            }
+        }
+
+        return valor;
     };
 
     $scope.quantidade = function (tipo) {
@@ -65,6 +86,20 @@ churras.controller('principal', function ($scope, $http) {
         }
 
         return qtd;
+    };
+
+    $scope.total = function (tipo) {
+        var total = 0;
+
+        for (var i = 0; i < $scope.compras.length; i++) {
+            var c = $scope.compras[i];
+
+            if (tipo === undefined || c.tipo === tipo) {
+                total += c.quantidade * c.valor;
+            }
+        }
+
+        return total.toFixed(2);
     };
 
 });
